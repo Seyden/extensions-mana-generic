363ee93d4bc2d7928030e53bdc78f97a4f019b22
Seyden <saiifii@live.de>
2024-12-15 09:27:14 +0100

    Webtoon | Add WebtoonDE, WebtoonEN, WebtoonES, WebtoonFR, WebtoonID, WebtoonID, WebtoonTH and WebtoonZH

---
+++ src/runners/Webtoon/WebtoonES.ts
@@ -0,0 +1,33 @@
+import { RunnerInfo } from '@suwatte/daisuke'
+
+import {
+    BASE_URL_XX,
+    MOBILE_URL_XX,
+    Webtoon,
+    WebtoonBaseInfo
+} from '../../templates/Webtoon/Webtoon'
+
+const SOURCE_NAME = 'WebtoonES'
+const LOCALE = 'es'
+const DATE_FORMAT = 'D MMM YYYY'
+const LANGUAGE = 'es-ES'
+const BASE_URL = `${BASE_URL_XX}/${LOCALE}`
+const MOBILE_URL = `${MOBILE_URL_XX}/${LOCALE}`
+const HAVE_TRENDING = false
+
+
+
+
+export class Target extends Webtoon {
+
+    info: RunnerInfo = {
+        id: SOURCE_NAME,
+        name: SOURCE_NAME,
+        ...WebtoonBaseInfo,
+        supportedLanguages: [LANGUAGE]
+    }
+
+    constructor (){
+        super(LOCALE, DATE_FORMAT, LANGUAGE, BASE_URL, MOBILE_URL, HAVE_TRENDING)
+    }
+}
---
+++ src/templates/Webtoon/WebtoonHelper.ts
@@ -0,0 +1,4 @@
+﻿export type FilterProps = {
+    genres?: string;
+    canvasgenres?: string;
+};
